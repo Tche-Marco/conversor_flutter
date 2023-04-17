3fad19549bd876f46dab263e16bbda2fdd7fd607
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-04-17 18:25:55 -0300

    Feat: Criando função para fazer request em API + estrutura inicial

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=b17824fc";
+
+void main() async {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(Uri.parse(request));
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$Conversor\$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: Container(), // próxima aula adicionar FutureBuilder
+    );
+  }
+}
